93c41ece759fbb6dd3c8beefc2fa6ca233bc30c9
lj-D-coder <lj_1990@live.com>
2023-12-08 12:31:01 +0530

    CREATEING LANDING SCREEN

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,40 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child:Column(
+            children: <Widget>[CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/lourembam.jpg'),
+            ),
+            Text(
+              'Jagadish',
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                fontStyle: FontStyle.italic,
+                fontSize: 25.0,
+                fontWeight: FontWeight.w800,
+                color: Colors.white,
+
+              ),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                fontFamily: 'Poppins',
+                color: Color.fromARGB(255, 182, 224, 221),
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+                
+                fontSize: 15.0
+              ),
+            )
+            ],
+          )
+        )
       ),
     );
   }
