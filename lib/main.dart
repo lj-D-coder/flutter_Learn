2dba78989eb67bccabcee81a35f45406d2205663
lj-D-coder <lj_1990@live.com>
2023-12-08 14:41:39 +0530

    created basic layout

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MyApp()
+   MyApp()
   );
 }
 
@@ -11,16 +11,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
+         body: SafeArea(
           child:Column(
-            children: <Widget>[CircleAvatar(
+             children: <Widget>[
+               CircleAvatar(
               radius: 50.0,
               backgroundImage: AssetImage('images/lourembam.jpg'),
             ),
-            Text(
+             Text(
               'Jagadish',
               style: TextStyle(
                 fontFamily: 'Poppins',
@@ -31,18 +32,65 @@ class MyApp extends StatelessWidget {
 
               ),
             ),
-            Text(
+             Text(
               "FLUTTER DEVELOPER",
               style: TextStyle(
                 fontFamily: 'Poppins',
                 color: Color.fromARGB(255, 182, 224, 221),
                 fontWeight: FontWeight.bold,
                 letterSpacing: 2.5,
-                
                 fontSize: 15.0
               ),
-            )
-            ],
+            ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsetsDirectional.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.phone,
+                    size:15.0,
+                    color: Color.fromARGB(255, 240, 34, 154),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('7005-294-063',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),    
+                ],
+              ), 
+              ),
+                Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsetsDirectional.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.email,
+                    size:10.0,
+                    color: Color.fromARGB(255, 240, 34, 154),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('jagdislourembam@gmail.com',
+                    style: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),    
+                ],
+              ), 
+              ),
+          ],
           )
         )
       ),
